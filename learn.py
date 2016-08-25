f3dbe30f2bc5cad05ddbdbc125b2a6f16fc5344d
王影 <wangying@everhigh.com.cn>
2016-08-25 22:21:51 +0800

    增加图片请求的headers

--- learn.py
+++ learn.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from urllib.request import urlopen
+from urllib import request
 from urllib.parse import urlsplit
 from os.path import basename
 from urllib.error import HTTPError
@@ -8,7 +9,7 @@ from bs4 import BeautifulSoup
 session = requests.Session()
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36",
            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-           "Cookie":"3661916355=d1e4xlIB80LuAx8UN9flfgiXK%2FLt7a%2FifpVwl8X23Q; _gat=1; 3661916355=1ad9Dgz6RH6oSB9wAsq8VKECtejKUtJuCP2jE4Wt1g; jdna=596e6fb28c1bb47f949e65e1ae03f7f5#1472021389774; Hm_lvt_fd93b7fb546adcfbcf80c4fc2b54da2c=1472019203,1472019540,1472021188,1472021391; Hm_lpvt_fd93b7fb546adcfbcf80c4fc2b54da2c=1472021391; _ga=GA1.2.1501173324.1463663963"}
+           "Cookie":"_gat=1; 711139627=6e16Lomm9yZybx0QbWFXR6kcHvt%2FD17YWg6tvaw4; jdna=596e6fb28c1bb47f949e65e1ae03f7f5#1472131824362; _ga=GA1.2.1849853486.1453643018; Hm_lvt_fd93b7fb546adcfbcf80c4fc2b54da2c=1471761239,1471788385,1471957084,1472131823; Hm_lpvt_fd93b7fb546adcfbcf80c4fc2b54da2c=1472131825"}
 
 file = open('./pages.txt','w')
 url = "http://jandan.net/ooxx"
@@ -18,16 +19,21 @@ address = bsObj.findAll("a",{"class":"previous-comment-page"})[0]
 imgs = bsObj.findAll("a",{"class":"view_img_link"})
 for x in imgs:
     try:
-        img_data = urlopen(x['href']).read()
+        reqImg = request.Request(x['href'])
+        reqImg.add_header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36")
+        print("正在下载"+x['href'])
+        img_data = urlopen(reqImg).read()
+        print("下载完成")
         file_name = basename(urlsplit(x['href'])[2])
-        output = open('img/'+file_name,'wb')
+        output = open('./img/'+file_name,'wb')
         output.write(img_data)
         output.close()
-    except:
-        pass
+        print("保存完成")
+    except HTTPError as e:
+        print(e)
 
 
-for x in range(100):
+for x in range(5):
     url = address['href']
     req = session.get(url,headers=headers)
     bsObj = BeautifulSoup(req.text,"lxml")
@@ -38,12 +44,17 @@ for x in range(100):
     imgs = bsObj.findAll("a",{"class":"view_img_link"})
     for x in imgs:
         try:
-            img_data = urlopen(x['href']).read()
+            reqImg = request.Request(x['href'])
+            reqImg.add_header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36")
+            print("正在下载" + x['href'])
+            img_data = urlopen(reqImg).read()
+            print("下载完成")
             file_name = basename(urlsplit(x['href'])[2])
             output = open('img/'+file_name,'wb')
             output.write(img_data)
             output.close()
-        except:
-            pass
+            print("保存完成")
+        except HTTPError as e:
+            print(e)
 
 
